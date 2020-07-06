0e89f227edb3b3d853e3dce0df5c4c8fd7650ccf
Pako Herrera <pakman_198@hotmail.com>
2020-07-06 12:01:39 -0700

    Added and configured storybook to document the components

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import { addParameter, addDecorator, addParameters } from '@storybook/react';
 import { withContexts } from '@storybook/addon-contexts/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
 
 import { contexts } from './context';
 
@@ -11,3 +13,5 @@ addParameters({
 });
 
 addDecorator(withContexts(contexts));
+addDecorator(withKnobs);
+addDecorator(withA11y);
